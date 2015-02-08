d368431b7f0e5e762c3d6d29fc0f5180282f4824
ioawnen <ioawnen@gmail.com>
2015-02-08 21:21:19 +0000

    Load of Android changes, Server changes

---
+++ mobile_android/HOMS-android/app/src/main/java/ioawnen/homs_android/Client.java
@@ -0,0 +1,17 @@
+package ioawnen.homs_android;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Ian on 05/02/2015.
+ */
+public class Client {
+
+   }
+
