90519610d709cfa24035cdbb29b42c605918f15e
ioawnen <ioawnen@gmail.com>
2015-04-17 22:06:07 +0100

    Client side work

--- client_generic/HOMS-client/src/Client.java
+++ client_generic/HOMS-client/src/Client.java
@@ -62,14 +62,246 @@ public class Client {
 		catch (Exception ex) { System.err.println(ex.getMessage()); return new String[] {"-20","Connection Error!"}; }
 	}
 
-	public String[] addUser (String creds[]) {
+	public String[] addUser (String creds[], String username, String password, int isActive, int isAdmin, int EmployeeNumber, String FirstName, String LastName) {
+		XmlRpcClient server = createConnection();
 
+		Vector params = new Vector();
+		params.add(creds[0]);
+		params.add(creds[1]);
+		params.add(username);
+		params.add(password);
+		params.add(isActive);
+		params.add(isAdmin);
+		params.add(EmployeeNumber);
+		params.add(FirstName);
+		params.add(LastName);
 
-		return new String[] {};
+		try {
+			Object result = server.execute("server.addUser", params);
+			String res = result.toString();
+			return strToArray(res);
+		}
+		catch (Exception ex) { System.err.println(ex.getMessage()); return new String[] {"-20","Connection Error!"}; }
+	}
+
+	public String[] modifyUser(String creds[], int U_Id, String username, String password, int isActive, int isAdmin, int EmployeeNumber, String FirstName, String LastName) {
+		XmlRpcClient server = createConnection();
+
+		Vector params = new Vector();
+		params.add(creds[0]);
+		params.add(creds[1]);
+		params.add(U_Id);
+		params.add(username);
+		params.add(password);
+		params.add(isActive);
+		params.add(isAdmin);
+		params.add(EmployeeNumber);
+		params.add(FirstName);
+		params.add(LastName);
+
+		try {
+			Object result = server.execute("server.modifyUser", params);
+			String res = result.toString();
+			return strToArray(res);
+		}
+		catch (Exception ex) { System.err.println(ex.getMessage()); return new String[] {"-20","Connection Error!"}; }
+	}
+
+	public String[] removeUser(String creds[], int U_Id) {
+		XmlRpcClient server = createConnection();
+
+		Vector params = new Vector();
+		params.add(creds[0]);
+		params.add(creds[1]);
+		params.add(U_Id);
+
+		try {
+			Object result = server.execute("server.removeUser", params);
+			String res = result.toString();
+			return strToArray(res);
+		}
+		catch (Exception ex) { System.err.println(ex.getMessage()); return new String[] {"-20","Connection Error!"}; }
+	}
+
+	public String[] addOrder(String[] creds, int T_Id) {
+		XmlRpcClient server = createConnection();
+
+		Vector params = new Vector();
+		params.add(creds[0]);
+		params.add(creds[1]);
+		params.add(T_Id);
+
+		try {
+			Object result = server.execute("server.addOrder", params);
+			String res = result.toString();
+			return strToArray(res);
+		}
+		catch (Exception ex) { System.err.println(ex.getMessage()); return new String[] {"-20","Connection Error!"}; }
+	}
+
+	public String[] modifyOrder(String[] creds, int O_Id, int T_Id) {
+		XmlRpcClient server = createConnection();
+
+		Vector params = new Vector();
+		params.add(creds[0]);
+		params.add(creds[1]);
+		params.add(O_Id);
+		params.add(T_Id);
+
+		try {
+			Object result = server.execute("server.modifyOrder", params);
+			String res = result.toString();
+			return strToArray(res);
+		}
+		catch (Exception ex) { System.err.println(ex.getMessage()); return new String[] {"-20","Connection Error!"}; }
+	}
+
+	public String[] removeOrder(String[] creds, int O_Id) {
+		XmlRpcClient server = createConnection();
+
+		Vector params = new Vector();
+		params.add(creds[0]);
+		params.add(creds[1]);
+		params.add(O_Id);
+
+		try {
+			Object result = server.execute("server.removeOrder", params);
+			String res = result.toString();
+			return strToArray(res);
+		}
+		catch (Exception ex) { System.err.println(ex.getMessage()); return new String[] {"-20","Connection Error!"}; }
+	}
+
+	public String[] getOrder(String[] creds, int O_Id) {
+		XmlRpcClient server = createConnection();
+
+		Vector params = new Vector();
+		params.add(creds[0]);
+		params.add(creds[1]);
+		params.add(O_Id);
+
+		try {
+			Object result = server.execute("server.getOrder", params);
+			String res = result.toString();
+			return strToArray(res);
+		}
+		catch (Exception ex) { System.err.println(ex.getMessage()); return new String[] {"-20","Connection Error!"}; }
+	}
+
+	public String[] getNOrders(String[] creds, int nOrders) {  //TODO: Return as 2d array {{CODE,STR},{ORDER SHIT},{ORDER SHIT}...}
+		XmlRpcClient server = createConnection();
+
+		Vector params = new Vector();
+		params.add(creds[0]);
+		params.add(creds[1]);
+		params.add(nOrders);
+
+		try {
+			Object result = server.execute("server.getNOrders", params);
+			String res = result.toString();
+			return strToArray(res);
+		}
+		catch (Exception ex) { System.err.println(ex.getMessage()); return new String[] {"-20","Connection Error!"}; }
+	}
+
+	public String[] getUserOrders(String[] creds, int nOrders) {
+		XmlRpcClient server = createConnection();
+
+		Vector params = new Vector();
+		params.add(creds[0]);
+		params.add(creds[1]);
+		params.add(nOrders);
+
+		try {
+			Object result = server.execute("server.getUserOrders", params);
+			String res = result.toString();
+			return strToArray(res);
+		}
+		catch (Exception ex) { System.err.println(ex.getMessage()); return new String[] {"-20","Connection Error!"}; }
+	}
+
+	public String[] getInProgressOrders(String[] creds, int nOrders) {
+		XmlRpcClient server = createConnection();
+
+		Vector params = new Vector();
+		params.add(creds[0]);
+		params.add(creds[1]);
+		params.add(nOrders);
+
+		try {
+			Object result = server.execute("server.getInProgressOrders", params);
+			String res = result.toString();
+			return strToArray(res);
+		}
+		catch (Exception ex) { System.err.println(ex.getMessage()); return new String[] {"-20","Connection Error!"}; }
+	}
+
+	public String[] addOrderItem(String[] creds, int O_Id, int I_Id) {
+		XmlRpcClient server = createConnection();
+
+		Vector params = new Vector();
+		params.add(creds[0]);
+		params.add(creds[1]);
+		params.add(O_Id);
+		params.add(I_Id);
+
+		try {
+			Object result = server.execute("server.addOrderItem", params);
+			String res = result.toString();
+			return strToArray(res);
+		}
+		catch (Exception ex) { System.err.println(ex.getMessage()); return new String[] {"-20","Connection Error!"}; }
+	}
+
+	public String[] modifyOrderItem(String[] creds, int O_Id, int I_Id, int isActive) {
+		XmlRpcClient server = createConnection();
+
+		Vector params = new Vector();
+		params.add(creds[0]);
+		params.add(creds[1]);
+		params.add(O_Id);
+		params.add(I_Id);
+		params.add(isActive);
+
+		try {
+			Object result = server.execute("server.modifyOrderItem", params);
+			String res = result.toString();
+			return strToArray(res);
+		}
+		catch (Exception ex) { System.err.println(ex.getMessage()); return new String[] {"-20","Connection Error!"}; }
+	}
+
+	public String[] removeOrderItem(String[] creds, int O_Id, int I_Id) {
+		XmlRpcClient server = createConnection();
+
+		Vector params = new Vector();
+		params.add(creds[0]);
+		params.add(creds[1]);
+		params.add(O_Id);
+		params.add(I_Id);
+
+		try {
+			Object result = server.execute("server.removeOrderItem", params);
+			String res = result.toString();
+			return strToArray(res);
+		}
+		catch (Exception ex) { System.err.println(ex.getMessage()); return new String[] {"-20","Connection Error!"}; }
 	}
 
 
-	public String[] drop_table_users() {
+
+
+
+
+
+
+
+
+
+
+
+
+	public String[] drop_table_users() { //gg moron
 		XmlRpcClient server = createConnection();
 
 		try {
